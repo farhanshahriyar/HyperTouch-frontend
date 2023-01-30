1dfb31f062a64782eca428b2462db931a340f089
farhanshahriyar <farhan.abir9999@gmail.com>
2023-01-30 20:23:15 +0600

    fixed bugs

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -44,9 +44,9 @@ const Footer = () => {
           <div className='box'>
             <h2>Contact Us</h2>
             <ul>
-              <li>70 Washington Square South, New York, NY 10012, United States </li>
-              <li>Email: uilib.help@gmail.com</li>
-              <li>Phone: +1 1123 456 780</li>
+              <li>Dhaka - 1214, Bangladesh </li>
+              <li>Email: support@hypertouch.com</li>
+              <li>Phone: +88015 3456 7894</li>
             </ul>
           </div>
         </div>
